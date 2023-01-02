95505ebdf3d1ff5ff233724331b018ccf0caa294
Kumar Shreyas <kumar.shreyas@leadsquared.com>
2023-01-02 15:31:14 +0530

    adds: buildspec file

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
-
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    let userEntity = this.repo.create({ email, password });
+    return this.repo.save(userEntity);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    let userFound = await this.repo.findOne({ where: { id } });
+    if (!userFound) {
+      throw 'User Not Found';
+    }
+    userFound = { ...userFound, ...attrs };
+    return this.repo.save(userFound);
+  }
+
+  async remove(id: number) {
+    let userFound = await this.repo.findOne({ where: { id } });
+    if (!userFound) {
+      throw 'User Not Found';
+    }
+    this.repo.remove(userFound);
+  }
+}
